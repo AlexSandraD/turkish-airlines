932bb858c5c486167e40704eedd70ed830c3ae67
AlexSandra <alexandradudyuk@gmail.com>
2019-09-11 13:22:25 +0300

    add loadind

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import { fetchTickets } from "../action/index";
+import classNames from "classnames";
 
 import logo from "../img/logo.svg";
 import turkishAirlines from "../img/turkish_airlines.svg";
@@ -103,8 +104,10 @@ export class HomePage extends React.Component {
   }
 
   render() {
-    const { tickets } = this.props;
+    const { tickets, loading } = this.props;
     const { rate, selected, selectedStops } = this.state;
+
+    console.log(loading);
 
     const newSelected = [...selectedStops];
 
@@ -127,146 +130,155 @@ export class HomePage extends React.Component {
     return (
       <div className="homepage">
         <header className="homepage-header">
-          <img src={logo} alt="logo" />
+          <img
+            src={logo}
+            alt="logo"
+            className={classNames("preloader", loading ? "wait" : "")}
+          />
         </header>
 
-        <section className="main">
-          <div className="filter-block">
-            <div className="filter-block-info">
-              <p>ВАЛЮТА</p>
+        {loading ? (
+          ""
+        ) : (
+          <section className="main">
+            <div className="filter-block">
+              <div className="filter-block-info">
+                <p>ВАЛЮТА</p>
 
-              <div className="radio-group">
-                <input
-                  type="radio"
-                  value="RUB"
-                  id="rub"
-                  checked={selected === "RUB"}
-                  onChange={this.handleChange}
-                  className="stv-radio-button"
-                />
-                <label htmlFor="rub">RUB</label>
-
-                <input
-                  type="radio"
-                  value="USD"
-                  id="usd"
-                  checked={selected === "USD"}
-                  onChange={this.handleChange}
-                  className="stv-radio-button"
-                />
-                <label htmlFor="usd">USD</label>
-
-                <input
-                  type="radio"
-                  value="EUR"
-                  id="eur"
-                  checked={selected === "EUR"}
-                  onChange={this.handleChange}
-                  className="stv-radio-button"
-                />
-                <label htmlFor="eur">EUR</label>
-              </div>
-
-              <p>КОЛИЧЕСТВО ПЕРЕСАДОК</p>
-
-              <ul className="filter-select">
-                <li id="wrapper">
+                <div className="radio-group">
                   <input
-                    className="styled-checkbox"
-                    onChange={this.removeSelected.bind(this)}
-                    checked={this.state.isAllchecked}
-                    type="checkbox"
-                    value="all"
-                    name="checkAll"
+                    type="radio"
+                    value="RUB"
+                    id="rub"
+                    checked={selected === "RUB"}
+                    onChange={this.handleChange}
+                    className="stv-radio-button"
                   />
-                  <label>Все</label>
-                  <p className="text">Только</p>
-                </li>
+                  <label htmlFor="rub">RUB</label>
 
-                {this.renderList()}
-              </ul>
+                  <input
+                    type="radio"
+                    value="USD"
+                    id="usd"
+                    checked={selected === "USD"}
+                    onChange={this.handleChange}
+                    className="stv-radio-button"
+                  />
+                  <label htmlFor="usd">USD</label>
+
+                  <input
+                    type="radio"
+                    value="EUR"
+                    id="eur"
+                    checked={selected === "EUR"}
+                    onChange={this.handleChange}
+                    className="stv-radio-button"
+                  />
+                  <label htmlFor="eur">EUR</label>
+                </div>
+
+                <p>КОЛИЧЕСТВО ПЕРЕСАДОК</p>
+
+                <ul className="filter-select">
+                  <li id="wrapper">
+                    <input
+                      className="styled-checkbox"
+                      onChange={this.removeSelected.bind(this)}
+                      checked={this.state.isAllchecked}
+                      type="checkbox"
+                      value="all"
+                      name="checkAll"
+                    />
+                    <label>Все</label>
+                    <p className="text">Только</p>
+                  </li>
+
+                  {this.renderList()}
+                </ul>
+              </div>
             </div>
-          </div>
 
-          <div>
-            {ticket.length > 0 &&
-              ticket.map(
-                (
-                  {
-                    origin,
-                    origin_name,
-                    destination,
-                    destination_name,
-                    departure_time,
-                    arrival_time,
-                    departure_date,
-                    arrival_date,
-                    stops,
-                    price
-                  },
-                  i
-                ) => (
-                  <div key={i} className="result-block">
-                    <div className="turkish-airlines">
-                      <img
-                        src={turkishAirlines}
-                        alt="turkish-airlines"
-                        className="turkish-airlines-img"
-                      />
-                      <button className="button-buy">
-                        Купить <br /> за{" "}
-                        {selected === "RUB"
-                          ? price + " ₽"
-                          : selected === "EUR"
-                          ? Math.ceil(price * rate.EUR) + " €"
-                          : Math.ceil(price * rate.USD) + " $"}
-                      </button>
-                    </div>
-                    <div className="turkish-airlines-info">
-                      <div className="info-first">
-                        <p className="info-time">{departure_time}</p>
-                        <div className="div-stops">
-                          {stops === 0 ? (
-                            ""
-                          ) : (
-                            <div className="info-stops">
-                              <p className="text-light">{stops}</p>
-                              <p className="text-light">
-                                {stops === 1 ? "ПЕРЕСАДКА" : "ПЕРЕСАДКИ"}
-                              </p>
-                            </div>
-                          )}
-                          <img src={fly} alt="fly" />
-                        </div>
-                        <p className="info-time">{arrival_time}</p>
+            <div>
+              {ticket.length > 0 &&
+                ticket.map(
+                  (
+                    {
+                      origin,
+                      origin_name,
+                      destination,
+                      destination_name,
+                      departure_time,
+                      arrival_time,
+                      departure_date,
+                      arrival_date,
+                      stops,
+                      price
+                    },
+                    i
+                  ) => (
+                    <div key={i} className="result-block">
+                      <div className="turkish-airlines">
+                        <img
+                          src={turkishAirlines}
+                          alt="turkish-airlines"
+                          className="turkish-airlines-img"
+                        />
+                        <button className="button-buy">
+                          Купить <br /> за{" "}
+                          {selected === "RUB"
+                            ? price + " ₽"
+                            : selected === "EUR"
+                            ? Math.ceil(price * rate.EUR) + " €"
+                            : Math.ceil(price * rate.USD) + " $"}
+                        </button>
                       </div>
-                      <div className="info-second">
-                        <div className="info-origin">
-                          <p className="text-dark">
-                            {origin}, {origin_name}
-                          </p>
-                          <p className="text-light">{departure_date}, Пт</p>
+                      <div className="turkish-airlines-info">
+                        <div className="info-first">
+                          <p className="info-time">{departure_time}</p>
+                          <div className="div-stops">
+                            {stops === 0 ? (
+                              ""
+                            ) : (
+                              <div className="info-stops">
+                                <p className="text-light">{stops}</p>
+                                <p className="text-light">
+                                  {stops === 1 ? "ПЕРЕСАДКА" : "ПЕРЕСАДКИ"}
+                                </p>
+                              </div>
+                            )}
+                            <img src={fly} alt="fly" />
+                          </div>
+                          <p className="info-time">{arrival_time}</p>
                         </div>
-                        <div className="info-origin">
-                          <p className="text-dark">
-                            {destination_name}, {destination}
-                          </p>
-                          <p className="text-light">{arrival_date}, Пт</p>
+                        <div className="info-second">
+                          <div className="info-origin">
+                            <p className="text-dark">
+                              {origin}, {origin_name}
+                            </p>
+                            <p className="text-light">{departure_date}, Пт</p>
+                          </div>
+                          <div className="info-origin">
+                            <p className="text-dark">
+                              {destination_name}, {destination}
+                            </p>
+                            <p className="text-light">{arrival_date}, Пт</p>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                )
-              )}
-          </div>
-        </section>
+                  )
+                )}
+            </div>
+          </section>
+        )}
       </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  tickets: state.tickets.tickets
+  tickets: state.tickets.tickets,
+  loading: state.tickets.loading
 });
 
 const mapDispatchToProps = dispatch =>
